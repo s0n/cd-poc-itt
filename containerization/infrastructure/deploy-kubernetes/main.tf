a96c918de530b0665c11496287dcc30b20e0b205
s0n <sngbango@yahoo.fr>
2024-01-04 10:44:54 +0100

    wip

--- containerization/infrastructure/deploy-kubernetes/main.tf
+++ containerization/infrastructure/deploy-kubernetes/main.tf
@@ -1,5 +1,8 @@
 provider "azurerm" {
   features {}
+
+  subscription_id   = var.az_secret.subsciption_id
+  tenant_id         = var.az_secret.tenant_id
 }
 
 data "azurerm_container_registry" "poc_registry" {
@@ -9,4 +12,12 @@ data "azurerm_container_registry" "poc_registry" {
 
 data "azurerm_resource_group" "arg" {
   name = "poc-devops-itt"
+}
+
+variable "az_secret" {
+  type = object({
+    tenant_id = string
+    subsciption_id = string
+  })
+  
 }
