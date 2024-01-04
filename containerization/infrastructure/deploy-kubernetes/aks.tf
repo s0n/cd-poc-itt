6e55433d3807139a27f4e78cae4fa637b1816aa5
s0n <sngbango@yahoo.fr>
2024-01-04 14:47:31 +0100

    wip

--- containerization/infrastructure/deploy-kubernetes/aks.tf
+++ containerization/infrastructure/deploy-kubernetes/aks.tf
@@ -1,8 +1,12 @@
+resource "azurerm_resource_group" "rg" {
+  name = "MyTFRssourceGroup"
+  location = "westus2"
+}
 
 resource "azurerm_kubernetes_cluster" "k8s_cluster" {
   name                = "aks_devop_cluster"
-  location            = data.azurerm_resource_group.arg.location
-  resource_group_name = data.azurerm_resource_group.arg.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "aksDevopClusterITT"
 
   default_node_pool {
@@ -19,14 +23,6 @@ resource "azurerm_kubernetes_cluster" "k8s_cluster" {
     Environment = "dev"
   }
 }
-
-/*
-resource "azurerm_role_assignment" "role_assignment" {
-  principal_id                     = azurerm_kubernetes_cluster.k8s_cluster.kubelet_identity[0].object_id
-  role_definition_name             = "AcrPull"
-  scope                            = data.azurerm_container_registry.poc_registry.id
-  skip_service_principal_aad_check = true
-}*/
 
 output "client_certificate" {
   value     = azurerm_kubernetes_cluster.k8s_cluster.kube_config.0.client_certificate
