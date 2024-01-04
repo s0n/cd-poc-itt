6701d4be340dae708f24f626de82dc89955087a9
s0n <sngbango@yahoo.fr>
2024-01-04 15:47:57 +0100

    wip

--- containerization/infrastructure/deploy-kubernetes/main.tf
+++ containerization/infrastructure/deploy-kubernetes/main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
   subscription_id   = "e4c9144b-b968-4637-ab69-8e867d33b201"
   tenant_id         = "faa9a254-14cd-419d-9c55-f11ae45ca9b8"
   client_id         = "4ef7c402-cf28-4739-b2f3-76a5d6cb0e5c"
-  client_secret     = var.client_secret
+  client_secret     = "7b28637c-9170-42e1-bc28-6a65f4abc910"
 }
 
 
