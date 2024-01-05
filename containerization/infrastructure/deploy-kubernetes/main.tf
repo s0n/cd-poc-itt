df28d2e5872f77d6366988893d4650a177865894
s0n <sngbango@yahoo.fr>
2024-01-05 14:24:25 +0100

    wip

--- containerization/infrastructure/deploy-kubernetes/main.tf
+++ containerization/infrastructure/deploy-kubernetes/main.tf
@@ -1,12 +1,48 @@
 provider "azurerm" {
   features {}
+
+  subscription_id = "<azure_subscription_id>"
+  tenant_id       = "faa9a254-14cd-419d-9c55-f11ae45ca9b8"
+  client_id       = "4ef7c402-cf28-4739-b2f3-76a5d6cb0e5c"
+  client_secret   = var.client_secret
 }
 
-/*data "azurerm_container_registry" "poc_registry" {
-  name                = "pocDevopsITT"
-  resource_group_name = "poc-devops-itt"
+variable "client_secret" {
+  type = string
 }
 
-data "azurerm_resource_group" "arg" {
-  name = "poc-devops-itt"
-}*/
+resource "azurerm_resource_group" "rg" {
+  name     = "MyTFRssourceGroup"
+  location = "westus2"
+}
+
+resource "azurerm_kubernetes_cluster" "k8s_cluster" {
+  name                = "aks_devop_cluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aksDevopClusterITT"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.k8s_cluster.kube_config.0.client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.k8s_cluster.kube_config_raw
+  sensitive = true
+}
